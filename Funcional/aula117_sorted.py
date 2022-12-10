9aa003e0b38149afc078fdf59d85e993198195ce
Julio Zampier <juliozampier@gmail.com>
2022-12-10 11:28:21 -0300

    Continue with class 117

--- Funcional/aula117_sorted.py
+++ Funcional/aula117_sorted.py
@@ -12,5 +12,18 @@ list_one = [3, 5, 1, 8, 4, 2]
 touple_one = (3, 5, 1, 8, 4, 2)
 
 list_one.sort()
-print(list_one)
-print(sorted(touple_one))
+print(list_one, '\n', "-" * 20)
+
+#! If we don't assign it to a variable, it will be consumed on print
+print(sorted(touple_one), '\n', "-" * 20)
+print(sorted(touple_one, reverse=True), '\n', "-" * 20)
+
+songs = [
+    {'title': 'Amigo', 'played': 4},
+    {'title': 'Ai se eu te pego', 'played': 45},
+    {'title': 'Para sempre', 'played': 12},
+    {'title': 'Nuvem de lagrimas', 'played': 23},
+    {'title': 'Papel Mache', 'played': 14},
+]
+most_played_songs = sorted(songs, key=lambda song: song.get('played'), reverse=True)
+print(most_played_songs, '\n', "-" * 20)
