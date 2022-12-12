fe2760606c6d4c7b145d5e6f2da907e7e20bdb83
Julio Zampier <juliozampier@gmail.com>
2022-12-12 10:23:03 -0300

    Add aula119 file

---
+++ Funcional/aula119_len_abs_sum_round.py
@@ -0,0 +1,29 @@
+"""Continuing integrated functions"""
+
+#! len()
+
+a = len('Uncle Julio is the best')
+
+#! abs()
+
+b = abs(-34.5)
+
+#! sum()
+
+s = sum([1, 2, 3, 4, 5, 6, 7])
+si = sum([1, 2, 3, 4, 5, 6, 7], 1000)
+
+#! round() or round(element, precision)
+
+va = round(3.1415)
+vacd = round(3.1415, 2)
+
+
+print(
+    f'a = {a},\
+ b = {b},\
+ s = {s},\
+ si = {si},\
+ va = {va},\
+ vacd = {vacd}'
+)
