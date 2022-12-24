c71bf10c7199b797d286b288bdae18d25412769d
Julio Zampier <juliozampier@gmail.com>
2022-12-24 19:42:05 -0300

    Add aula131 (introduction to databases)

---
+++ Databases_w_python/aula131_database.py
@@ -0,0 +1,28 @@
+"""Databases
+- Holding information
+- Webservers
+
+- File system
+- SGBD (Interface between user and database)
+- Colusmns (fields) , rolls (data)
+- Metadata (data that represents data)
+
+-Main reasons
+    - Controll redundancy
+    - Data sharing
+    - Data Independency
+    - Security
+    - Backup and failure restoration
+    - Force integrity restriction
+    - Increasing productivity level
+    - Flexbility and standardization
+    
+-Where do we find them
+    - Almost everywhere
+
+-Relational database
+-NoSQL database
+-Data Store (Big data)
+
+
+"""
