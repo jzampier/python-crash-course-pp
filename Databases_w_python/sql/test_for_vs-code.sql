0a9c191dbaac3afca84b3cf4c4111f53b8291c64
Julio Zampier <juliozampier@gmail.com>
2023-01-02 11:36:32 -0300

    Add some VScode Queries

---
+++ Databases_w_python/sql/test_for_vs-code.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE test(
+--     empid decimal(10) NOT NULL UNIQUE PRIMARY KEY,
+--     empname char(20) NOT NULL,
+--     empemail char(50) NOT NULL
+-- )
+
+-- INSERT INTO test
+-- VALUES (1, 'Julio', 'juliozampier@gmail.com');
+
+SELECT * FROM test;
